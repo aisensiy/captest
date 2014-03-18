a2ee3a8872eee651175321e90fd840bfe04fbc49
aisensiy <aisensiy@gmail.com>
2014-03-18 12:35:11 +0800

    init commit for cap test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Captest::Application.config.secret_key_base = '36eb11f03862e978673131455954c0004c11091125c9757f64175c8b7e07963af27d3f36e5110f5fe801dd8352f50ac21ab6adbb2cd79a11e4e9131be5122889'
